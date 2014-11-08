10f5c390a536305a5f821d22f36759e43032129e
Pavel Ravits <vim55k@gmail.com>
2014-11-08 18:33:47 +0000

    2

--- package.js
+++ package.js
@@ -9,6 +9,5 @@ Package.onUse(function (api, where) {
   api.versionsFrom('1.0');
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
   api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
-  if (api.export)
-  api.export('Errors');
+  if (api.export) api.export('Errors');
 });
